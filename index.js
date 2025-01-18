f87038996e0ffb3200f2d0a9c164defea2ad0350
Anezijane <ahnnieanezi@gmail.com>
2025-01-18 12:58:09 +0100

    create routes for products

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const MemberModel = require('./models/member')
+const Product = require('./models/product');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -13,6 +15,7 @@ const app = express()
 app.use(express.json()) 
 app.use(cors())
 
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 mongoose.connect(uri) 
 
 const db = mongoose.connection 
@@ -20,6 +23,60 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
     console.log('Connected to database')
 })
+
+app.post('/api/products', async (req, res) => {
+    try {
+      const product = new Product(req.body);
+      await product.save();
+      res.status(201).json(product);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.get('/api/products/:id', async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      if (product) {
+        res.status(200).json(product);
+      } else {
+        res.status(404).json({ message: 'Product not found' });
+      }
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.put('/api/products/:id', async (req, res) => {
+    try {
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      res.status(200).json(updatedProduct);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+    try {
+      const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+      if (deletedProduct) {
+        res.status(200).json({ message: 'Product deleted' });
+      } else {
+        res.status(404).json({ message: 'Product not found' });
+      }
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
 
 app.post("/login", async (req, res) => {
     const { email, password } = req.body;
